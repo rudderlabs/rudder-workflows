b1e2be4db4ee316ccb4d83ceec83fdfbb8b7b4af
rahulkumark <rahulkumarkorada@gmail.com>
2023-06-07 19:09:41 +0530

    test changes

--- .github/scripts/get-yaml-diff.js
+++ .github/scripts/get-yaml-diff.js
@@ -1,23 +1,8 @@
 const { Octokit } = require('@octokit/rest');
-const yaml = require('js-yaml');
 const octokit = new Octokit({ auth: process.env.GITHUB_TOKEN });
 const owner = process.env.GITHUB_REPOSITORY.split('/')[0];
 const repo = process.env.GITHUB_REPOSITORY.split('/')[1];
 const prNumber = process.env.GITHUB_REF.split('/')[2];
-
-// Recursive function to extract keys
-function extractKeys(obj, keys) {
-    let values = [];
-    for (let key in obj) {
-        if (obj.hasOwnProperty(key) && keys.includes(key)) {
-            values.push(obj[key]);
-        }
-        if (typeof obj[key] === 'object') {
-            values = values.concat(extractKeys(obj[key], keys));
-        }
-    }
-    return values;
-}
 
 async function run() {
     try {
@@ -29,23 +14,8 @@ async function run() {
 
         for (const file of files) {
             if (!file.filename.includes('/') && (file.filename.endsWith('.yaml') || file.filename.endsWith('.yml'))) {
-                const { data: contents } = await octokit.repos.getContent({
-                    owner,
-                    repo,
-                    path: file.filename,
-                    ref: 'refs/pull/' + prNumber + '/head'
-                });
-
-                const content = Buffer.from(contents.content, 'base64').toString();
-                const doc = yaml.load(content);  // changed from yaml.safeLoad to yaml.load
-
-                const keysToExtract = ['image', 'tag', 'version', 'repository'];
-                let extractedValues = extractKeys(doc, keysToExtract);
-
-                // ensure the values are unique
-                let uniqueValues = [...new Set(extractedValues)];
-
-                console.log(`Values from ${file.filename}: `, uniqueValues);
+                let changes = extractChangesFromPatch(file.patch);
+                console.log(`Changes in file ${file.filename}:`, changes);
             }
         }
     } catch (error) {
@@ -53,4 +23,73 @@ async function run() {
     }
 }
 
+function extractChangesFromPatch(patch) {
+    let changes = [];
+    const lines = patch.split('\n');
+    for (let line of lines) {
+        if (line.startsWith('+') && !line.startsWith('+++')) {
+            changes.push(line.slice(1).trim());
+        }
+    }
+    return changes;
+}
+
 run();
+
+
+// const { Octokit } = require('@octokit/rest');
+// const yaml = require('js-yaml');
+// const octokit = new Octokit({ auth: process.env.GITHUB_TOKEN });
+// const owner = process.env.GITHUB_REPOSITORY.split('/')[0];
+// const repo = process.env.GITHUB_REPOSITORY.split('/')[1];
+// const prNumber = process.env.GITHUB_REF.split('/')[2];
+
+// // Recursive function to extract keys
+// function extractKeys(obj, keys) {
+//     let values = [];
+//     for (let key in obj) {
+//         if (obj.hasOwnProperty(key) && keys.includes(key)) {
+//             values.push(obj[key]);
+//         }
+//         if (typeof obj[key] === 'object') {
+//             values = values.concat(extractKeys(obj[key], keys));
+//         }
+//     }
+//     return values;
+// }
+
+// async function run() {
+//     try {
+//         const { data: files } = await octokit.pulls.listFiles({
+//             owner,
+//             repo,
+//             pull_number: prNumber
+//         });
+
+//         for (const file of files) {
+//             if (!file.filename.includes('/') && (file.filename.endsWith('.yaml') || file.filename.endsWith('.yml'))) {
+//                 const { data: contents } = await octokit.repos.getContent({
+//                     owner,
+//                     repo,
+//                     path: file.filename,
+//                     ref: 'refs/pull/' + prNumber + '/head'
+//                 });
+
+//                 const content = Buffer.from(contents.content, 'base64').toString();
+//                 const doc = yaml.load(content);  // changed from yaml.safeLoad to yaml.load
+
+//                 const keysToExtract = ['image', 'tag', 'version', 'repository'];
+//                 let extractedValues = extractKeys(doc, keysToExtract);
+
+//                 // ensure the values are unique
+//                 let uniqueValues = [...new Set(extractedValues)];
+
+//                 console.log(`Values from ${file.filename}: `, uniqueValues);
+//             }
+//         }
+//     } catch (error) {
+//         console.error("Error: ", error);
+//     }
+// }
+
+// run();
